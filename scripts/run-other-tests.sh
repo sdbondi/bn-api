7727962dfe5e0f2d3d28522be8ae3ae3c93a7eee
Stanimal <sdbondi@users.noreply.github.com>
2019-02-04 17:34:21 +0200

    fix

--- scripts/run-other-tests.sh
+++ scripts/run-other-tests.sh
@@ -2,10 +2,6 @@
 # Ensure we are in the root of the git repo
 # cd $(git rev-parse --show-toplevel)
 
-pwd
-
-ls -lrta
-
 cd db
 cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
 cd ..
