1413a609e5063d710dfa445c38db3e76b62f0ba9
Stanimal <sdbondi@users.noreply.github.com>
2019-02-05 15:58:01 +0200

    fix

--- scripts/run-other-tests.sh
+++ scripts/run-other-tests.sh
@@ -2,7 +2,5 @@
 # Ensure we are in the root of the git repo
 # cd $(git rev-parse --show-toplevel)
 
-cd db
-cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
-cd ..
+./target/release/bndb_cli create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
 cargo test --release --exclude bigneon_api --all
