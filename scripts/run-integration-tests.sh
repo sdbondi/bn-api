e72ee3af965b93deeef550709d6b02d5eb140516
Stanimal <sdbondi@users.noreply.github.com>
2019-02-05 14:33:34 +0200

    fix

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -3,12 +3,22 @@
 # Ensure we are in the root of the git repo
 #cd $(git rev-parse --show-toplevel)
 cd db
-cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883 || {
-    echo "Migrations failed"
-    exit 1
-}
+
+if [[ -f ./target/release/bndb_cli ]]; then
+    echo "----- Reusing built bndb_cli -----"
+    ./target/release/bndb_cli create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883 || {
+        echo "Migrations failed"
+        exit 1
+    }
+else
+    cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883 || {
+        echo "Migrations failed"
+        exit 1
+    }
+fi
 
 if [[ -f ./target/release/server ]]; then
+    echo "----- Reusing built server -----"
     ./target/release/server -t false &
     export SERVER_PID=$!$1
 else
@@ -22,7 +32,7 @@ fi
 newman run --timeout-request 60000 ../integration-tests/bigneon-tests.postman_collection.json -e ../integration-tests/travis.postman_environment.json
 NEWMAN_EXIT_CODE=$?
 kill -s SIGTERM $SERVER_PID
-if [ $NEWMAN_EXIT_CODE -ne 0 ]
+if [[ $NEWMAN_EXIT_CODE -ne 0 ]]
 then
     exit $NEWMAN_EXIT_CODE
 fi
