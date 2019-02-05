a3462091449c838f8fa369adb6990a631b067669
Stanimal <sdbondi@users.noreply.github.com>
2019-02-05 13:47:05 +0200

    fix

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Ensure we are in the root of the git repo
-cd $(git rev-parse --show-toplevel)
+#cd $(git rev-parse --show-toplevel)
 cd db
 cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883 || {
     echo "Migrations failed"
@@ -11,13 +11,8 @@ cd ../api
 cargo build --release
 cargo run --release -- -t false &
 export SERVER_PID=$!$1
-# Run newman tests
-apt-get install nodejs
-apt-get install npm
-    # Workaround for invalid NPM certificate
-npm config set strict-ssl false
-npm install -g newman
 
+# Run newman tests
 newman run --timeout-request 60000 ../integration-tests/bigneon-tests.postman_collection.json -e ../integration-tests/travis.postman_environment.json
 NEWMAN_EXIT_CODE=$?
 kill -s SIGTERM $SERVER_PID
