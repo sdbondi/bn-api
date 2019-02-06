4c06ac739070b50c1d785927079f64bdcd709514
Stanimal <sdbondi@users.noreply.github.com>
2019-02-06 13:18:04 +0200

    fix

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -6,11 +6,12 @@ new_version=$APP_VERSION
 
 function bump_patch {
     local file="$1"
+    local version=`sed -En 's/version[[:space:]]*=[[:space:]]*"([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\1/p' < $file`
     local search='^(version[[:space:]]*=[[:space:]]*).+'
     local replace="\1\"${new_version}\""
 
     sed -i.tmp -E "s/${search}/${replace}/g" "$1"
-    echo "$file bumped from $version to $new_version"
+    echo "$file set to $new_version from $version"
     rm "$1.tmp"
 }
 
