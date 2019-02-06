c4754f042800fea81e26780b87825dac3789fa04
Stanimal <sdbondi@users.noreply.github.com>
2019-02-06 13:15:30 +0200

    fix

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -2,5 +2,22 @@
 
 set -e
 
-echo "I WOULD SET THE VERSION TO $APP_VERSION"
+new_version=$APP_VERSION
+
+function bump_patch {
+    local file="$1"
+    local search='^(version[[:space:]]*=[[:space:]]*).+'
+    local replace="\1\"${new_version}\""
+
+    sed -i.tmp -E "s/${search}/${replace}/g" "$1"
+    echo "$file bumped from $version to $new_version"
+    rm "$1.tmp"
+}
+
+FILES=( "db/Cargo.toml" "api/Cargo.toml" )
+
+for target in "${FILES[@]}"; do
+    bump_patch "$target"
+done
+
 
