7f739a8a6e03a008d06cb74f9f899a231fa43a33
Stanimal <sdbondi@users.noreply.github.com>
2019-02-07 16:31:45 +0200

    fix

--- scripts/tag-and-commit.sh
+++ scripts/tag-and-commit.sh
@@ -13,7 +13,7 @@ if [[ -z "$APP_VERSION" ]]; then
 fi
 
 mkdir -p $HOME/.ssh/
-declare -r SSH_FILE="$(mktemp -ut $HOME/.ssh/githubXXX)"
+declare -r SSH_FILE="$(mktemp -ut $HOME/.ssh/githubXXXXXX)"
 
 echo -n $GITHUB_SSH_KEY > $SSH_FILE
 
