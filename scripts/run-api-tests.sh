49e04b9a2e97de6722511d71cdf8731cf7493518
Stanimal <sdbondi@users.noreply.github.com>
2019-02-08 07:53:38 +0200

    fix

--- scripts/run-api-tests.sh
+++ scripts/run-api-tests.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# Ensure we are in the root of the git repo
-#cd $(git rev-parse --show-toplevel)
 
 ./target/release/bndb_cli create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
 cd api && cargo test --release
